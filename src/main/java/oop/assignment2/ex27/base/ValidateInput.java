7e6774f113fea602fa4f4cf5de68086a61c315d8
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-06-13 23:47:18 -0400

    Update ValidateInput.java

--- src/main/java/oop/assignment2/ex27/base/ValidateInput.java
+++ src/main/java/oop/assignment2/ex27/base/ValidateInput.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
 public class ValidateInput
 {
     public class inputValidation
