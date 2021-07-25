55ee739911d9728b565d68e8d145447a2fc9ce27
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 11:14:03 -0400

    Create GuessingGameTest.java

---
+++ src/test/java/oop/assignment2/ex32/base/GuessingGameTest.java
@@ -0,0 +1,13 @@
+package oop.assignment2.ex32.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessingGameTest {
+
+    @Test
+    void captureGuess() {
+        //unsure on testing
+    }
+}
