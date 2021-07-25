5a7f9aa87842dda13d392def9cf9f131c33f9631
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 11:13:56 -0400

    Create MultiplicationTableTest.java

---
+++ src/test/java/oop/assignment2/ex30/base/MultiplicationTableTest.java
@@ -0,0 +1,12 @@
+package oop.assignment2.ex30.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplicationTableTest
+{ @Test
+public void MultiplicationTable_test() {
+    // N/A Test Case not applicable for program, just prints table
+}
+}
