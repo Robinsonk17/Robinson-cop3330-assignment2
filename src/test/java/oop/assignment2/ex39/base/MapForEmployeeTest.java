bb050b16eb014fff7002f33be60ad3a729516b90
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 11:14:25 -0400

    Create MapForEmployeeTest.java

---
+++ src/test/java/oop/assignment2/ex39/base/MapForEmployeeTest.java
@@ -0,0 +1,12 @@
+package oop.assignment2.ex39.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapForEmployeeTest {
+    @Test
+    void maptest() {
+        // Cant figure how to test map
+    }
+}
