85f6b7ccbe9f6b9b2de6b9501631f2ebcd751bd9
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 11:14:17 -0400

    Create PasswordGeneratorTest.java

---
+++ src/test/java/oop/assignment2/ex37/base/PasswordGeneratorTest.java
@@ -0,0 +1,13 @@
+package oop.assignment2.ex37.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordGeneratorTest {
+
+    @Test
+    void passwordGen() {
+        // No test case needed, password is random
+    }
+}
